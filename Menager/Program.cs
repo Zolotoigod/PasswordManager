0a91d78ce5352469d83e18b9d4012d0e3cb2c607
vladshalkevich@gmail.com <vladshalkevich@gmail.com>
2022-05-17 19:57:34 +0300

    Add project files.

---
+++ Menager/Program.cs
@@ -0,0 +1,9 @@
+﻿
+using MenagerCore;
+using System.Security.Cryptography;
+
+var key = Console.ReadLine();
+using Aes newAes = Aes.Create();
+await ImportExport.WriteToFile(key, Core.Incrypted(Core.GeneratePass(), newAes.Key, newAes.IV));
+
+Console.WriteLine((Core.Decrypted(await ImportExport.ReadFromFile(key), newAes.Key, newAes.IV)));
---
+++ PasswordManagerLibrary/Core.cs
@@ -0,0 +1,106 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace MenagerCore
+{
+    public static class Core
+    {
+        private const string collection = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789@#$%&";
+
+        public static string GeneratePass(int count = 12)
+        {
+            Random random = new Random();
+            StringBuilder pass = new StringBuilder();
+
+            for (int i = 0; i < count; i++)
+            {
+                pass.Append(collection[random.Next(0, 67)]);
+            }
+            
+            Console.WriteLine(pass.ToString());
+
+            return pass.ToString();
+        }
+
+        public static byte[] Incrypted(string pass, byte[] Key, byte[] IV)
+        {
+            if (pass == null || pass.Length <= 0)
+                throw new ArgumentNullException("plainText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+            byte[] encrypted;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create an encryptor to perform the stream transform.
+                ICryptoTransform encryptor = aesAlg.CreateEncryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for encryption.
+                using (MemoryStream msEncrypt = new MemoryStream())
+                {
+                    using (CryptoStream csEncrypt = new CryptoStream(msEncrypt, encryptor, CryptoStreamMode.Write))
+                    {
+                        using (StreamWriter swEncrypt = new StreamWriter(csEncrypt))
+                        {
+                            //Write all data to the stream.
+                            swEncrypt.Write(pass);
+                        }
+                        encrypted = msEncrypt.ToArray();
+                    }
+                }
+            }
+
+            // Return the encrypted bytes from the memory stream.
+            return encrypted;
+        }
+
+        public static string Decrypted(byte[] cipherText, byte[] Key, byte[] IV)
+        {
+            if (cipherText == null || cipherText.Length <= 0)
+                throw new ArgumentNullException("cipherText");
+            if (Key == null || Key.Length <= 0)
+                throw new ArgumentNullException("Key");
+            if (IV == null || IV.Length <= 0)
+                throw new ArgumentNullException("IV");
+
+            // Declare the string used to hold
+            // the decrypted text.
+            string pass;
+
+            // Create an Aes object
+            // with the specified key and IV.
+            using (Aes aesAlg = Aes.Create())
+            {
+                aesAlg.Key = Key;
+                aesAlg.IV = IV;
+
+                // Create a decryptor to perform the stream transform.
+                ICryptoTransform decryptor = aesAlg.CreateDecryptor(aesAlg.Key, aesAlg.IV);
+
+                // Create the streams used for decryption.
+                using (MemoryStream msDecrypt = new MemoryStream(cipherText))
+                {
+                    using (CryptoStream csDecrypt = new CryptoStream(msDecrypt, decryptor, CryptoStreamMode.Read))
+                    {
+                        using (StreamReader srDecrypt = new StreamReader(csDecrypt))
+                        {
+
+                            // Read the decrypted bytes from the decrypting stream
+                            // and place them in a string.
+                            pass = srDecrypt.ReadToEnd();
+                        }
+                    }
+                }
+            }
+
+            return pass;
+        }
+    }
+}
