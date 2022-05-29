aeae0c344a004dac010e5d3df12146caea1d62ab
Vlad <vladshalkevich@gmail.com>
2022-05-29 22:23:24 +0300

    Added logick of change pages, customization

--- WPFPassManager/MainWindow.xaml.cs
+++ WPFPassManager/MainWindow.xaml.cs
@@ -23,11 +23,53 @@ namespace WPFPassManager
         public MainWindow()
         {
             InitializeComponent();
+            this.Loaded += MainWindowLoad;
+        }
+        private void ToLoder_Click(object sender, RoutedEventArgs e)
+        {
+            Menu.Visibility = Visibility.Hidden;
+            ToMenu.Visibility = Visibility.Visible;
+            PassRead.Visibility = Visibility.Visible;
+        }
+        private void ToGenerator_Click(object sender, RoutedEventArgs e)
+        {
+            Menu.Visibility = Visibility.Hidden;
+            PassGenerator.Visibility = Visibility.Visible;
+            ToMenu.Visibility = Visibility.Visible;
         }
 
-        private void Submit_Click(object sender, RoutedEventArgs e)
+        private void ToCustom_Click(object sender, RoutedEventArgs e)
+        {
+            Menu.Visibility = Visibility.Hidden;
+            PassCustom.Visibility = Visibility.Visible;
+            ToMenu.Visibility = Visibility.Visible;
+        }
+
+        private void ToMenu_Click(object sender, RoutedEventArgs e)
+        {
+            ToMenu.Visibility = Visibility.Hidden;
+            PassGenerator.Visibility = Visibility.Hidden;
+            PassCustom.Visibility = Visibility.Hidden;
+            PassRead.Visibility = Visibility.Hidden;
+            Menu.Visibility = Visibility.Visible;
+        }
+
+        private void Generate_Click(object sender, RoutedEventArgs e)
         {
 
+        }
+
+        private void Copy_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void MainWindowLoad(object sender, RoutedEventArgs e)
+        {
+            ToMenu.Visibility = Visibility.Hidden;
+            PassGenerator.Visibility = Visibility.Hidden;
+            PassCustom.Visibility = Visibility.Hidden;
+            PassRead.Visibility = Visibility.Hidden;
         }
     }
 }
